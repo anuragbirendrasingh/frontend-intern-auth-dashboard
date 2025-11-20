d32cb3245515c99517eb3b5ea1fb22cdbdcbd080
Anurag Birendra Singh <anuragbirendrasingh@gmail.com>
2025-11-20 23:51:13 +0530

    created the tasks controller

---
+++ Back_End/controller/taskController.js
@@ -0,0 +1,97 @@
+const Task = require("../models/Task");
+
+const createTask = async (req, res) => {
+  try {
+    const { title, description, status } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: "Title is required" });
+    }
+    const task = await Task.create({
+      title,
+      description,
+      status,
+      createdBy: req.user._Id, // from protect middleware
+    });
+
+    res.status(201).json({ message: "Task Created SuccessFully", task });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getTasks = async (req, res) => {
+  try {
+    const { search, status } = req.query;
+
+    let filter = { createdBy: req.user._id };
+
+    if (search) {
+      filter.title = { $regex: search, $options: "i" };
+    }
+
+    if (status) {
+      filter.status = status;
+    }
+
+    const tasks = await Task.find(filter).sort({ createdAt: -1 });
+
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const task = await Task.findOne({
+      _id: id,
+      createdBy: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      message: "Task updated successfully",
+      task: updatedTask,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const task = await Task.findOne({
+      _id: id,
+      createdBy: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    await Task.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+};
