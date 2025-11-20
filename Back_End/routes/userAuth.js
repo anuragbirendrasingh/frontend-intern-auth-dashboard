94ad27a27a3ab8f7ba8fc9d2ea899fe17310536e
Anurag Birendra Singh <anuragbirendrasingh@gmail.com>
2025-11-20 15:25:17 +0530

    written the logic for validated the data through API_Level

--- Back_End/routes/userAuth.js
+++ Back_End/routes/userAuth.js
@@ -1,6 +1,7 @@
 const express = require("express") ;
 const router = express.Router();
 const User = require("../models/Users") ;
+const {validateSignUpData} = require("../utils/validation")
 
 
 
---
+++ Back_End/utils/validation.js
@@ -0,0 +1,20 @@
+const validator = require("validator");
+
+const validateSignUpData = (req) => {
+  const { firstName, emailId, password, age } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error(`please enter your firstName & lastName`);
+  } else if (firstName.length < 3 || firstName.length > 10) {
+    throw new Error("Please enter first name between 3-10 characters");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("please enter valid email");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("please enter a strong password");
+  } else if (age < 15) {
+    throw new Error("You are too younger too access the poratal ");
+  } else if (age > 120) {
+    throw new Error("please enter a valid age ");
+  }
+};
+
+module.exports = { validateSignUpData };
