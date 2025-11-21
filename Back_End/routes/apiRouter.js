da91d13efd73766255ad2b0fad8611c62fdf10c0
Anurag Birendra Singh <anuragbirendrasingh@gmail.com>
2025-11-21 13:13:24 +0530

    added the routes of tasks

--- Back_End/routes/apiRouter.js
+++ Back_End/routes/apiRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router   = express.Router();
 const userAuth = require("./userAuth")
+const taskRoutes = require("../routes/taskRoutes")
 
 
 router.use('/auth',userAuth);
-
+router.use('/task',taskRoutes);
 
 module.exports = router ;
--- Back_End/routes/taskRoutes.js
+++ Back_End/routes/taskRoutes.js
@@ -3,3 +3,18 @@ const router = express.Router();
 
 const {  createTask,getTasks,updateTask,deleteTask} = require("../controller/taskController");
 
+const { protect } = require("../middleware/authMiddleware");
+
+
+router.post("/createTask", protect, createTask);
+
+
+router.get("/getTasks", protect, getTasks);
+
+
+router.put("/updateTask/:id", protect, updateTask);
+
+
+router.delete("/delteTask/:id", protect, deleteTask);
+
+module.exports = router;
