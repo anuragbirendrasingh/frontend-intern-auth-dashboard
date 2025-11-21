8045dc0bafb31068ad26e5d1a8a11d7e9773a327
Anurag Birendra Singh <anuragbirendrasingh@gmail.com>
2025-11-21 13:26:42 +0530

    setUp the react

---
+++ Front_End/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Hello</h1>
+
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+    </>
+  )
+}
+
+export default App
